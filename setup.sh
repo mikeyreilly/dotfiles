307d4ebd8519d417487d45f9caef2ad06f76d3ad
Michael Reilly <reillym@localhost.localdomain>
2020-12-20 10:58:27 +0000

    fix tmux copy and paste for tmux 3.1

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 mkdir -p ~/.config/i3
 mkdir -p ~/bin
-rm ~/.Xmodmap
-rm ~/.Xresources
-rm ~/.config/i3/config
-rm ~/.tmux.conf
-rm ~/.zsh_prefs
+rm -f ~/.Xmodmap
+rm -f ~/.Xresources
+rm -f ~/.config/i3/config
+rm -f ~/.tmux.conf
+rm -f ~/.zsh_prefs
 rm ~/bin/exit-menu
 ln -s ~/dotfiles/.zsh_prefs $HOME/.zsh_prefs
 ln -s ~/dotfiles/.Xmodmap $HOME/.Xmodmap
